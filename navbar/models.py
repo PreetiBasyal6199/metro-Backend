b08ba9a7f0c39a20526bb722e8e6d8229d00a5db
Preeti <nanubasyal@gmail.com>
2021-11-30 10:08:18 -0500

    Final Commit

--- navbar/models.py
+++ navbar/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class navbar_items(models.Model):
     navText=models.CharField(max_length=50)
-    link=models.CharField(max_length=100)
+    link=models.CharField(max_length=100,blank=True)
 
 
     def __str__(self):
-        return self.nav_title
+        return self.navText
