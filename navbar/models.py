e3f570b2b5070b8deba5b53dcb6d4af5bcc1d704
Preeti <nanubasyal@gmail.com>
2021-11-30 10:02:13 -0500

    Models title changed

--- navbar/models.py
+++ navbar/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class navbar_items(models.Model):
-    nav_title=models.CharField(max_length=50)
-    nav_link=models.URLField(max_length=100)
+    navText=models.CharField(max_length=50)
+    link=models.CharField(max_length=100)
 
 
     def __str__(self):
