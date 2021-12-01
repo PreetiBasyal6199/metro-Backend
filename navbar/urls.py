93a63bdf310630427b5e25a2e224074b1f2860d4
Preeti <nanubasyal@gmail.com>
2021-11-30 20:47:51 -0500

    requirement.txt updated

--- navbar/urls.py
+++ navbar/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns=[
      path('nav-items/', navbar_View),
      path('nav-items/<int:pk>/', update_navbar_View),
-     path('background-items/', views.background_View)
+     path('background-item/', views.background_View)
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
