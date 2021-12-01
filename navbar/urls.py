fabe0440ff52abbb2adb0c55ebb5a4c0f013bba3
Preeti <nanubasyal@gmail.com>
2021-11-30 21:26:41 -0500

    Views  updated

--- navbar/urls.py
+++ navbar/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
 from .views import navbar_View,update_navbar_View
+from .views import background_view
 from . import views
 urlpatterns=[
      path('nav-items/', navbar_View),
      path('nav-items/<int:pk>/', update_navbar_View),
-     path('background-item/', views.background_View)
+     path('background-item/', views.background_View),
+     path('background-items/',background_view.as_view() ),
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- navbar/views.py
+++ navbar/views.py
@@ -63,3 +63,22 @@ def background_View(request):
         return JsonResponse(serializer.errors, status=400)
 
 
+
+
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import backgroundSerializer
+
+class background_view(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, format=None):
+        serializer = backgroundSerializer(request.files,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
