f4ada69e16e1a20b05db336a52fb1a3efc8899c9
Preeti <nanubasyal@gmail.com>
2021-11-30 20:27:55 -0500

    requirement.txt updated

--- navbar/urls.py
+++ navbar/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from .views import navbar_View,update_navbar_View
 from . import views
 urlpatterns=[
      path('nav-items/', navbar_View),
      path('nav-items/<int:pk>/', update_navbar_View),
-     path('background-items/', views.background_View),
+     path('background-items/', views.background_View)
 
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
