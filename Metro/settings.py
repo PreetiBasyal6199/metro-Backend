8f41e000f8428e030cecf12db9834e3a4e2f0982
Preeti <nanubasyal@gmail.com>
2021-12-03 17:04:51 -0500

    Final

--- Metro/settings.py
+++ Metro/settings.py
@@ -139,7 +139,7 @@ MEDIA_URL = '/media/' # 'http://myhost:port/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Activate Django-Heroku.
