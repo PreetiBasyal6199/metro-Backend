be7e55170ae1cac898dc001c610dcc8408236c20
Preeti <nanubasyal@gmail.com>
2021-12-03 16:43:51 -0500

    Final

--- Metro/settings.py
+++ Metro/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =["*"]
+ALLOWED_HOSTS =["https://metro99-app.herokuapp.com/","localhost"]
 
 
 # Application definition
