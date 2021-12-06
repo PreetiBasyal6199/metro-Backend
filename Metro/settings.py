c24bf84029b7915bca053946cce01480bc5c22fa
Preeti <nanubasyal@gmail.com>
2021-12-06 09:38:46 -0500

    Final

--- Metro/settings.py
+++ Metro/settings.py
@@ -129,17 +129,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATICFILES_DIRS=[os.path.join(BASE_DIR,'staticfiles')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = '/staticfiles/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
+# MEDIA_URL = '/staticfiles/images/' # 'http://myhost:port/media/'
 STATIC_URL = '/static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/' # 'http://myhost:port/media/'
+
+MEDIA_URL = '/staticfiles/images/'
+
+  
+
+if DEBUG:
+
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+
+  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+  
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
 # Activate Django-Heroku.
